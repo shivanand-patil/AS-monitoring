5e9f65af0b17d769da59636bcbc0eefb94f803d9
Shivanand Patil <70444072+shivanand-patil@users.noreply.github.com>
2024-05-28 19:31:57 +0530

    ed

--- load.sh
+++ load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aerolab attach shell --name dc1 --node=1 -- asbench -p 3100 -n
-aerolab attach shell --name dc1 --node=2 -- asbench -p 3101 -n 
+aerolab attach shell --name dc1 --node=1 -- asbench -p 3100 -n test
+aerolab attach shell --name dc1 --node=2 -- asbench -p 3101 -n test
 
-aerolab attach shell --name dc2 --node=1 -- asbench -p 3102 -n 
+aerolab attach shell --name dc2 --node=1 -- asbench -p 3102 -n test
