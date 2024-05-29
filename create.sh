1d0ae6e2cf39c4878e36947d17a0be9ced78c9c6
shivanand-patil <shivanandpatil963@gmail.com>
2024-05-29 19:00:16 +0530

    prometheus port exposed

--- create.sh
+++ create.sh
@@ -8,7 +8,7 @@ aerolab xdr connect -S dc1 -D dc2 -M test
 aerolab cluster add exporter -n dc1 -o ape1.toml
 aerolab cluster add exporter -n dc2 -o ape2.toml
 
-aerolab client create ams -n ams -s dc1,dc2 -e 3000:3000
+aerolab client create ams -n ams -s dc1,dc2 -e 3000:3000 -e 9090:9090
 aerolab client attach  -n ams -- grafana-cli plugins install grafana-polystat-panel
 aerolab client attach  -n ams -- grafana-cli plugins install jdbranham-diagram-panel
 aerolab client attach  -n ams -- service grafana-server restart
